a54be8ec7a227bf809a42cf5884c9b7f575d9f65
Tracy Cano <tracycano3.14@gmail.com>
2024-12-06 01:02:26 -0800

    final changes

--- exercise.rb
+++ exercise.rb
@@ -18,22 +18,17 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
     return 0 if nth <= 0
   
-    fib_sequence = [1, 1]
-    sum = 0
-  
-    while fib_sequence.length < nth
-      next_number = fib_sequence[-1] + fib_sequence[-2]
-      fib_sequence << next_number
-    end
-  
-    fib_sequence.each do |num|
-      sum += num if num.even?
-    end
-  
-    sum
+  a, b = 1, 1
+  sum = 0
+
+  while b <= nth
+    sum += b if b.even?
+    a, b = b, a + b
+  end
+
+  sum
   end
 
 end
